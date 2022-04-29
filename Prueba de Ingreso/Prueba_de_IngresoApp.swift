a61b0ece62e21c56df524b74cae20f5fd6dccabb
arnnalddo <arnnalddo@icloud.com>
2022-04-29 13:16:27 -0400

    Initial Commit

---
+++ Prueba de Ingreso/Prueba_de_IngresoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prueba_de_IngresoApp.swift
+//  Prueba de Ingreso
+//
+//  Created by Arnaldo Alfredo on 2022-04-29.
+//
+
+import SwiftUI
+
+@main
+struct Prueba_de_IngresoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
